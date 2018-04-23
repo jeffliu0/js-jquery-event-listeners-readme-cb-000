f236e8278ee883f9af8c3d6bac7e021132261d6a
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-23 06:36:21 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -24,8 +24,7 @@ function pressIt(){
 function submitIt(){
   $('form').on('submit', function(){
     alert('your form is going to be submitted now'
-
-  })
+  }))
 }
 
 $(document).ready(function(){
