965e7bbad048a43bbf1633d1fbca4a64c5fbc5d9
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-23 06:31:36 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -14,7 +14,7 @@ function frameIt(){
 }
 
 function pressIt(){
-  $('input').on('keydown', function(key){
+  $('form input').on('keydown', function(key){
     if(key.which == 71){
       alert('g was pressed')
     }
